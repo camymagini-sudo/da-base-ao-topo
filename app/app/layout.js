0d7449c0864cf9eef7a9943c5ea603ee0c427260
camymagini-sudo <camymagini@gmail.com>
2026-05-24 22:14:18 -0300

    Create layout.js

---
+++ app/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Da Base ao Topo',
+  description: 'O Mundo Corporativo Real',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
